12ddde5ad429b503251cd1004f643a28b26910a3
Markus <vernetzt@gmx.net>
2014-02-17 19:49:02 +0100

    Umstrukturierung (siehe Beschreibung)
    + Entfernung der Klasse Clienthandling , da nun die Funktion wesentlich
    differenzierter geproggt wird
    
    + Hinzufügen einer neuen Klasse Authentifizierung. Sie wird in Zukunft
    das Anmelden einfach lösen. Aufrufen einer methode und zack! Momentan
    noch im Bau befindlich.
    
    + Umschreiben der Sinatralinks in Kleinschrift, da dies bei URI’s eher
    der Erwartung entspricht aka „Wer schreibt schon Links in der
    Adressleiste des Browsers groß?“ So stellen wir eine einfache
    Handhabung sicher und können Fehler vermeiden.
    
    + Auslagern von Funktionscode aus unserer momentanen main() Datei
    „sinatra.rb“ in die Klasse „Fotozugriff“. Hier befinden sich nun alle
    Methoden die dies beabsichtigen.
    
    + erste Schritte zu einer Grundstruktur durch anlegen von Ordnern
    speziell dem „Funktionsklassen“. Bitte für weitere Struktur sorgen und
    auf sie acht geben.
    
    + Fehlerbereinigung und Stabilitätsverbesserung

---
+++ Funktionsklassen/Authentifizierung.rb
@@ -0,0 +1,34 @@
+# hier sollte eigentlich die funktionen rein aus der
+# sinatra datei klappt aber net da ruby einfach
+# scheisse kompliziert ist wenn man mit mehreren dateien arbeitet
+
+=begin
+Autor : Markus Orlikowski
+Datum : 17.01.2014
+Bedeutung: Diese Klasse ermöglicht den Login bei flickr auf ganz einfache Weise
+=end
+
+require 'flickraw'
+
+class Authentifizierung
+	@flickrAccessToken
+	@flickrAccessSecret
+	
+	
+	
+	#Nach dem Aufruf von compute soll die Anmeldung stehen
+	#Sie fasst den Prozess in einer Methode zusammen
+	def compute()
+	
+	end
+	
+	def getFlickrAccesstoken()
+	
+	end
+	
+	def getFlickrAccessSecret()
+	
+	end
+	
+	
+end
--- sinatra.rb
+++ sinatra.rb
@@ -1,119 +1,65 @@
 require 'flickraw'
 require 'sinatra'
 require 'erb'
-
-#----------------- Authentication --------------------------------------
-
-FlickRaw.api_key="cf109011ace10a7068593a1cac50c3cb"
-FlickRaw.shared_secret="9db5e517217ee3f0"
+require './Funktionsklassen/Fotozugriff.rb'
 
 
-#--------------- Anmelden mit Benutzer ---------------------------------
-
-flickr.access_token = "72157640916575304-a2ef387d72b02f04"
-flickr.access_secret = "20d289d72c262b40"
-
-
-#--------- Holt sich alle Bild Ids vom dem Benutzer -------------------
-
-def getPhotos()
-@photos = flickr.photos.search(:user_id => "117480828@N08")
-
-#puts @photos.count # ausgabe in konsole um zugucken ob er was gefunden hat
-
-@photo_id = []
-
-     @photos.each do |p|                    
-	 @photo_id.push p.id
-     end
-
-  return @photo_id 
-end
-
-
-#---------Informationen für die einzelnen Bilder Speichern-------
-
-def getPhotosInfos()
-  @photo_id = getPhotos()
-  @infos = []
-
-    @photo_id.each do |pi|            
-      @infos.push  flickr.photos.getInfo(:photo_id => pi)  	 
-    end 
-
-  return @infos
-end
-
-
-#-----------------Bild hochladen------------------------
-
-def uploadPicture(title,pictureLink,description)
-
-  photo_path=pictureLink
-  flickr.upload_photo photo_path, :title => title, :description => description
-  redirect "/Gallery"
-
-end
-
-
-#-----------------Bild Löschen--------------------------
-
-def deletePicture(photoId)
-
-  flickr.photos.delete(:photo_id => photoId)
-
-end
+# --- Initialisieren ------------------------------------
+# Objekt für den Umgang mit den Fotografien und der Gallerie ------------
+foto = Fotozugriff.new
 
 
 #--------------Sinatra----------------------------------
 
-get "/Login" do
+
+
+get "/login" do
   erb :login
 end
 
 
 # Zeigt die Gallery vom Benutzer an
-get "/Gallery" do
-  @photosInfos = getPhotosInfos()
+get "/gallery" do
+  @photosInfos = foto.getPictureInfo()
     erb :gallery
 end
 
 # 
-get "/Album" do
+get "/album" do
  puts "Album"
 end
 
 
 # Einzelne Bilder anzeigen in Groß
-get "/Photo/:photoid" do
+get "/photo/:photoid" do
   @photo = flickr.photos.getInfo(:photo_id => "#{params[:photoid]}")
    erb :photo
 end
 
 
 # Formular für Bilder Hochladen
-get "/Upload" do
+get "/upload" do
  erb :upload
 end
 
 
 # Formular für Bilder Hochladen
-get "/Deleted/:photoid" do
- deletePicture("#{params[:photoid]}")
+get "/deleted/:photoid" do
+ foto.deletePicture("#{params[:photoid]}")
  @message="Picture is deleted"
  erb :response
 end
 
 
 # Bild vom Formular wird auf flickr Hochgeladen
-post "/Upload" do
+post "/upload" do
  @title = params[:title]
  @pictureLink = params[:pictureLink][:tempfile]
  @description = params[:description]
 
- uploadPicture(@title,@pictureLink,@description)
+ foto.uploadPicture(@title,@pictureLink,@description)
 
- redirect "/Gallery"
+ redirect "/gallery"
 end
 
 
