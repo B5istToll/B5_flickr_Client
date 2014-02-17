f239bb051171dada7c06e4edbf2e8ce3faa95a65
B5istToll <vernetzt@gmx.net>
2014-02-17 13:19:36 +0100

    Viele änderungen
    zuviel um alles aufzuschreiben.

---
+++ controller.rb
@@ -0,0 +1,3 @@
+# hier sollte eigentlich die funktionen rein aus der
+# sinatra datei klappt aber net da ruby einfach
+# scheisse kompliziert ist wenn man mit mehreren dateien arbeitet
--- sinatra.rb
+++ sinatra.rb
@@ -2,30 +2,118 @@ require 'flickraw'
 require 'sinatra'
 require 'erb'
 
+#----------------- Authentication --------------------------------------
 
-get "/index" do
-clientObj = Clienthandling.new
+FlickRaw.api_key="cf109011ace10a7068593a1cac50c3cb"
+FlickRaw.shared_secret="9db5e517217ee3f0"
 
-puts "urls"
-puts clientObj.getPhotosUrls()
 
-#@photos = photos
+#--------------- Anmelden mit Benutzer ---------------------------------
 
-##@picurls = 
-@a = clientObj.getPhotosUrls()
-@picurls = []
- @a.each do |p|            
-   @picurls.push p	 
- end 
-	
-puts "index"
-puts @picurls
+flickr.access_token = "72157640916575304-a2ef387d72b02f04"
+flickr.access_secret = "20d289d72c262b40"
 
-  erb :html
+
+#--------- Holt sich alle Bild Ids vom dem Benutzer -------------------
+
+def getPhotos()
+@photos = flickr.photos.search(:user_id => "117480828@N08")
+
+#puts @photos.count # ausgabe in konsole um zugucken ob er was gefunden hat
+
+@photo_id = []
+
+     @photos.each do |p|                    
+	 @photo_id.push p.id
+     end
+
+  return @photo_id 
 end
 
 
+#---------Informationen für die einzelnen Bilder Speichern-------
+
+def getPhotosInfos()
+  @photo_id = getPhotos()
+  @infos = []
+
+    @photo_id.each do |pi|            
+      @infos.push  flickr.photos.getInfo(:photo_id => pi)  	 
+    end 
+
+  return @infos
+end
 
 
+#-----------------Bild hochladen------------------------
+
+def uploadPicture(title,pictureLink,description)
+
+  photo_path=pictureLink
+  flickr.upload_photo photo_path, :title => title, :description => description
+  redirect "/Gallery"
+
+end
+
+
+#-----------------Bild Löschen--------------------------
+
+def deletePicture(photoId)
+
+  flickr.photos.delete(:photo_id => photoId)
+
+end
+
+
+#--------------Sinatra----------------------------------
+
+get "/Login" do
+  erb :login
+end
+
+
+# Zeigt die Gallery vom Benutzer an
+get "/Gallery" do
+  @photosInfos = getPhotosInfos()
+    erb :gallery
+end
+
+# 
+get "/Album" do
+ puts "Album"
+end
+
+
+# Einzelne Bilder anzeigen in Groß
+get "/Photo/:photoid" do
+  @photo = flickr.photos.getInfo(:photo_id => "#{params[:photoid]}")
+   erb :photo
+end
+
+
+# Formular für Bilder Hochladen
+get "/Upload" do
+ erb :upload
+end
+
+
+# Formular für Bilder Hochladen
+get "/Deleted/:photoid" do
+ deletePicture("#{params[:photoid]}")
+ @message="Picture is deleted"
+ erb :response
+end
+
+
+# Bild vom Formular wird auf flickr Hochgeladen
+post "/Upload" do
+ @title = params[:title]
+ @pictureLink = params[:pictureLink][:tempfile]
+ @description = params[:description]
+
+ uploadPicture(@title,@pictureLink,@description)
+
+ redirect "/Gallery"
+end
 
 
