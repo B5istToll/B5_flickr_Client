b74abbaef02158872dcc7afebe06afc25abad826
B5istToll <vernetzt@gmx.net>
2014-02-18 22:38:23 +0100

    Funktionen in die Fotoklasse hinzugefügt

--- sinatra.rb
+++ sinatra.rb
@@ -1,7 +1,8 @@
 require 'flickraw'
 require 'sinatra'
 require 'erb'
-require_relative './Funktionsklassen/Fotozugriff.rb'
+require_relative('Funktionsklassen/Fotozugriff')
+require_relative('Funktionsklassen/Authentifizierung')
 
 
 # --- Initialisieren ------------------------------------
@@ -21,109 +22,7 @@ flickr.access_token = "72157640916575304-a2ef387d72b02f04"
 flickr.access_secret = "20d289d72c262b40"
 
 
-#--------- Holt sich alle Bild Ids vom dem Benutzer -------------------
 
-def getPhotos()
-@photos = flickr.photos.search(:user_id => "117480828@N08")
-
-@photo_id = []
-
-     @photos.each do |p|                    
-	 @photo_id.push p.id
-     end
-
-  return @photo_id 
-end
-
-
-#---------Informationen für die einzelnen Bilder Speichern-------
-
-def getPhotosInfos()
-  @photo_id = getPhotos()
-  @infos = []
-
-    @photo_id.each do |pi|            
-      @infos.push  flickr.photos.getInfo(:photo_id => pi)  	 
-    end 
-
-  return @infos
-end
-
-
-#-----------------Bild hochladen------------------------
-
-def uploadPicture(title,pictureLink,description)
-
-  photo_path=pictureLink
-  flickr.upload_photo photo_path, :title => title, :description => description
-  redirect "/gallery"
-
-end
-
-
-#-----------------Bild Löschen--------------------------
-
-def deletePicture(photoId)
-
-  flickr.photos.delete(:photo_id => photoId)
-
-end
-
-
-#---------------Alben Holen-----------------------------
-
-def getPhotosets()
-
-  @Photosets = flickr.photosets.getList(:user_id => "117480828@N08")
-
-  return @Photosets
-end
-
-
-#---------------Alben Bilder Holen-----------------------------
-
-def getPhotosetPhotos(photosetId)
-
-  @photosetPhotos = flickr.photosets.getPhotos(:photoset_id => photosetId)
-
-  return @photosetPhotos
-end
-
-
-#--------------Album erstellen--------------------------
-
-def createPhotoset(title,primaryPicture,description)
-
-  flickr.photosets.create(:title => title, :primary_photo_id => primaryPicture, :description => description)
-
-end
-
-
-#----------Bild aus Album löschen------------------------
-
-def removePictureFromPhotoset(photosetid,photoId)
-
-  flickr.photosets.removePhoto(:photoset_id => photosetid,:photo_id => photoId)
-
-end
-
-
-#--------------Album löschen-----------------------------
-
-def deletePhotoset(photosetid)
-
-  flickr.photosets.delete(:photoset_id => photosetid)
-
-end
-
-
-#--------------Bild zum Album hinzufügen-----------------
-
-def addPhotoToPhotoset(photosetid,photoId)
-
-  flickr.photosets.addPhoto(:photoset_id => photosetid,:photo_id => photoId)
-
-end
 
 
 #--------------Sinatra----------------------------------
@@ -135,21 +34,21 @@ end
 
 # Zeigt die Gallery vom Benutzer an
 get "/gallery" do
-  @photosInfos = getPhotosInfos()
+  @photosInfos = foto.getPhotosInfos()
     erb :gallery
 end
 
 
 # Zeigt die Alben vom Benutzer an 
 get "/photosets" do
- @photosets = getPhotosets()
+ @photosets = foto.getPhotosets()
   erb :album
 end
 
 
 # Einzelne Bilder anzeigen in Groß
 get "/photoset/:photosetid" do
-  @photosetPhotos = getPhotosetPhotos("#{params[:photosetid]}")
+  @photosetPhotos = foto.getPhotosetPhotos("#{params[:photosetid]}")
    erb :album_gallery
 end
 
@@ -169,7 +68,7 @@ end
 
 # Formular für Bilder Hochladen
 get "/deleted/:photoid" do
- deletePicture("#{params[:photoid]}")
+ foto.deletePicture("#{params[:photoid]}")
  @message="Picture is deleted"
  erb :response
 end
@@ -181,7 +80,7 @@ post "/upload" do
  @pictureLink = params[:pictureLink][:tempfile]
  @description = params[:description]
 
- uploadPicture(@title,@pictureLink,@description)
+ foto.uploadPicture(@title,@pictureLink,@description)
 
  redirect "/gallery"
 end
@@ -189,7 +88,7 @@ end
 
 # Formular für Album erstellen
 get "/create_photoset" do
- @photosInfos = getPhotosInfos()
+ @photosInfos = foto.getPhotosInfos()
  erb :create_photoset
 end
 
@@ -199,14 +98,14 @@ post "/create_photoset" do
  @title = params[:title]
  @primaryPicture = params[:primaryPicture]
  @description = params[:description]
- createPhotoset(@title,@primaryPicture,@description)
+ foto.createPhotoset(@title,@primaryPicture,@description)
  redirect "/photosets"
 end
 
 
 # Bild aus Album entfernen
 get "/remove/:photosetid/:photoid"  do
- removePictureFromPhotoset("#{params[:photosetid]}","#{params[:photoid]}")
+ foto.removePictureFromPhotoset("#{params[:photosetid]}","#{params[:photoid]}")
  @message="Picture is removed"
  erb :response
 end
@@ -214,7 +113,7 @@ end
 
 # Album Löschen
 get "/delete_photoset/:photosetid"  do
- deletePhotoset("#{params[:photosetid]}")
+ foto.deletePhotoset("#{params[:photosetid]}")
  @message="Photoset is removed"
  erb :response
 end
@@ -222,7 +121,7 @@ end
 
 # Bilder zum hinzufügen für die Alben anzeigen
 get "/add/:photosetid"  do
-  @photosInfos = getPhotosInfos()
+  @photosInfos = foto.getPhotosInfos()
   @photosetId = "#{params[:photosetid]}"
     erb :add
 end
@@ -230,6 +129,6 @@ end
 
 # Bild zum Album hinzufügen
 get "/add/:photosetid/:photoid"  do
-  addPhotoToPhotoset("#{params[:photosetid]}","#{params[:photoid]}")
+  foto.addPhotoToPhotoset("#{params[:photosetid]}","#{params[:photoid]}")
     redirect "/photoset/#{params[:photosetid]}"
 end
