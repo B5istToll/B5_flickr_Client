ea9f6009aa9559a2a4f8ebf00f48eac8c73c9c58
B5istToll <vernetzt@gmx.net>
2014-02-20 14:01:13 +0100

    Formular Fehler abfrage hinzugefügt

--- Funktionsklassen/Authentifizierung.rb
+++ Funktionsklassen/Authentifizierung.rb
@@ -1,18 +1,23 @@
 =begin
 Autor : Markus Orlikowski
 Datum : 17.01.2014
-Bedeutung: Diese Klasse ermöglicht den Login bei flickr auf ganz einfache Weise
+Bedeutung: Diese Klasse ermöglicht den Login bei flickr auf ganz einfache Weise.
+
+Schritte des Authorisierungsprozesses
+1.) Anfrage-Token abrufen ( get_request_token() )
+2.) Autorisierung des Benutzers abrufen ( flickr.get_authorize_url(reqToken['oauth_token'], :perms => 'delete') )
+3.) Anforderungs-Token gegen Zugriffs-Token austauschen ( get_access_token(reqToken['oauth_token'], reqToken['oauth_token_secret'], verify) )
 =end
 
 require 'flickraw'
 require 'launchy'
 
 class Authentifizierung
-	  # Diese Werte erhält man direkt von flickr. Sie sind fix
-	  @flickrApiKey
-	  @flickrApiSecret
+	  # Diese Werte erhält man direkt von flickr. Sie sind fix.
+	  @flickrApiKey # Jede App hat einen eindeutigen Schlüssel von flickr zugeteilt bekommen.
+	  @flickrApiSecret # Jede App hat einen eindeutigen Schlüssel von flickr zugeteilt bekommen.
 
-	  #Diese Werte bekommt man durch Methodenanfragen
+	  #Diese Werte bekommt man durch Methodenabfragen.
 	  @flickrAccessToken
 	  @flickrAccessSecret
 	  @AuthURL
@@ -26,18 +31,35 @@ class Authentifizierung
 		@flickrApiKey = "88b3faf325e25878ff6c6c6a0d616fc7"
 		@flickrApiSecret = "48f5f79faa401c9f"
 		
-		compute()
-
-		#@authTokenFull = flickr.auth.getFullToken()
-		#@userID = @authTokenFull.user.nsid
+		#Die User-ID abspeichern
+		tmpUserID = flickr.people.findByUsername("username" => 'susi.sorglos61')
+		@userID = tmpUserID['nsid']
 	end
 	
+=begin
+	def initialize(username)
+		# Diese Werte erhält man direkt von flickr
+		FlickRaw.api_key="88b3faf325e25878ff6c6c6a0d616fc7" # Ist ein fixer Wert der pro Programm einmal vergeben wird. Er ist immer gleich
+		FlickRaw.shared_secret="48f5f79faa401c9f" # Ist ein fixer Wert der pro Programm einmal vergeben wird. Er ist immer gleich
+		@flickrApiKey = "88b3faf325e25878ff6c6c6a0d616fc7"
+		@flickrApiSecret = "48f5f79faa401c9f"
+		
+		#Die User-ID abspeichern
+		tmpUserID = flickr.people.findByUsername("username" => username)
+		@userID = tmpUserID['nsid']
+	end
+=end
+
 	#Nach dem Aufruf von compute soll die Anmeldung stehen
 	#Sie fasst den Prozess in einer Methode zusammen
 	def compute()
+		
 		reqToken = flickr.get_request_token()
 		@AuthURL = flickr.get_authorize_url(reqToken['oauth_token'], :perms => 'delete')
+		
+		#Öffnet die Seite für die Authorsierung im Standardbrowser
 		openURI(@AuthURL)
+		puts("Bitte Verifizierungskennung der Webseite hier 1:1 eintippen: ")
 		verify = gets.strip() # Code von der Webseite nach der Genehmigung
 
 		begin
@@ -74,12 +96,8 @@ class Authentifizierung
 
 	#öffnet die URI für die Genehmigung im Standardbrowser
 	def openURI(uri)
-    Launchy.open(uri)
+		Launchy.open(uri)
 	end
 	
 
 end
-
-
-#authObj = Authentifizierung.new()
-#puts(authObj.getUserID())
--- Funktionsklassen/Fotozugriff.rb
+++ Funktionsklassen/Fotozugriff.rb
@@ -1,123 +1,122 @@
-#Diese Klasse enthält Methoden mit denen wir in unserem Client auf die Fotos zugreifen.
+=begin
+Autor: Markus Orlikowski, Waldemar Kowalenko
+Datum: 01/2014
+Beschreibung : Diese Klasse enthält Methoden mit denen wir in unserem Client auf die Fotos zugreifen.
+=end
 
 class Fotozugriff
 
-#--------- Holt sich alle Bild Ids vom dem Benutzer -------------------
+	#Liefert die NSID des Benutzers
+	@authObj
+	
+	
+	def initialize()
+		@authObj = Authentifizierung.new()
+	end
 
-def getPhotos()
-@photos = flickr.photos.search(:user_id => "117480828@N08")
+	#--------- Holt sich alle Bild Ids vom dem Benutzer -------------------
+	def getPhotos()
+	@photos = flickr.photos.search(:user_id => @authObj.getUserID())
 
-@photo_id = []
+	@photo_id = []
 
-     @photos.each do |p|                    
-	 @photo_id.push p.id
-     end
+		 @photos.each do |p|                    
+		 @photo_id.push p.id
+		 end
 
-  return @photo_id 
-end
-
-
-#---------Informationen für die einzelnen Bilder Speichern-------
-
-def getPhotosInfos()
-  @photo_id = getPhotos()
-  @infos = []
-
-    @photo_id.each do |pi|            
-      @infos.push  flickr.photos.getInfo(:photo_id => pi)  	 
-    end 
-
-  return @infos
-end
+	  return @photo_id 
+	end
 
 
-#-----------------Bild hochladen------------------------
+	#---------Informationen für die einzelnen Bilder Speichern-------
 
-def uploadPicture(title,pictureLink,description)
+	def getPhotosInfos()
+	  @photo_id = getPhotos()
+	  @infos = []
+
+		@photo_id.each do |pi|            
+		  @infos.push  flickr.photos.getInfo(:photo_id => pi)  	 
+		end 
+
+	  return @infos
+	end
+
+
+	#-----------------Bild hochladen------------------------
+
+	def uploadPicture(title,pictureLink,description)
 
   photo_path=pictureLink
   flickr.upload_photo photo_path, :title => title, :description => description
 
+	  photo_path=pictureLink
+	  flickr.upload_photo photo_path, :title => title, :description => description
+
+	end
 
 
+	#-----------------Bild Löschen--------------------------
+
+	def deletePicture(photoId)
+
+	  flickr.photos.delete(:photo_id => photoId)
+
+	end
 
 
+	#---------------Alben Holen-----------------------------
+
+	def getPhotosets()
+
+	  @Photosets = flickr.photosets.getList(:user_id => @authObj.getUserID())
+
+	  return @Photosets
+	end
 
 
+	#---------------Alben Bilder Holen-----------------------------
+
+	def getPhotosetPhotos(photosetId)
+
+	  @photosetPhotos = flickr.photosets.getPhotos(:photoset_id => photosetId)
+
+	  return @photosetPhotos
+	end
 
 
+	#--------------Album erstellen--------------------------
+
+	def createPhotoset(title,primaryPicture,description)
+
+	  flickr.photosets.create(:title => title, :primary_photo_id => primaryPicture, :description => description)
+
+	end
 
 
+	#----------Bild aus Album löschen------------------------
+
+	def removePictureFromPhotoset(photosetid,photoId)
+
+	  flickr.photosets.removePhoto(:photoset_id => photosetid,:photo_id => photoId)
+
+	end
 
 
+	#--------------Album löschen-----------------------------
+
+	def deletePhotoset(photosetid)
+
+	  flickr.photosets.delete(:photoset_id => photosetid)
+
+	end
 
 
+	#--------------Bild zum Album hinzufügen-----------------
 
-end
+	def addPhotoToPhotoset(photosetid,photoId)
 
+	  flickr.photosets.addPhoto(:photoset_id => photosetid,:photo_id => photoId)
 
-#-----------------Bild Löschen--------------------------
-
-def deletePicture(photoId)
-
-  flickr.photos.delete(:photo_id => photoId)
-
-end
-
-
-#---------------Alben Holen-----------------------------
-
-def getPhotosets()
-
-  @Photosets = flickr.photosets.getList(:user_id => "117480828@N08")
-
-  return @Photosets
-end
-
-
-#---------------Alben Bilder Holen-----------------------------
-
-def getPhotosetPhotos(photosetId)
-
-  @photosetPhotos = flickr.photosets.getPhotos(:photoset_id => photosetId)
-
-  return @photosetPhotos
-end
-
-
-#--------------Album erstellen--------------------------
-
-def createPhotoset(title,primaryPicture,description)
-
-  flickr.photosets.create(:title => title, :primary_photo_id => primaryPicture, :description => description)
-
-end
-
-
-#----------Bild aus Album löschen------------------------
-
-def removePictureFromPhotoset(photosetid,photoId)
-
-  flickr.photosets.removePhoto(:photoset_id => photosetid,:photo_id => photoId)
-
-end
-
-
-#--------------Album löschen-----------------------------
-
-def deletePhotoset(photosetid)
-
-  flickr.photosets.delete(:photoset_id => photosetid)
-
-end
-
-
-#--------------Bild zum Album hinzufügen-----------------
-
-def addPhotoToPhotoset(photosetid,photoId)
-
-  flickr.photosets.addPhoto(:photoset_id => photosetid,:photo_id => photoId)
-
-end
+	end
 
 end
