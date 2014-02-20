6c7d1f1e48fc115540312d517406e35bf0fae4bc
B5istToll <vernetzt@gmx.net>
2014-02-20 20:21:20 +0100

    Sessions, Button und diverse GUI Verbesserungen
    + So meine Freunde, wir arbeiten nun mit Sinatrasessions
    + Es ist nun möglich einfach die Hauptadresse localhost:4567 aufzurufen
    und das Programm leitet dann weiter um
    + Rote ominöse Links habe ich durch verständliche Button ersetzt
    + response.erb habe ich gelöscht und leite nun nach einem Löschen
    direkt zurück zu Galerie um, wie man es erwarten würde, wenn etwas
    verschwindet
    + Die Hintergrundfarbe wurde angepasst und nun wirken die Fotos
    harmonischer
    + Zwischen Inhalt und Menü befindet sich nun ein Abstand. Er ist nicht
    mehr so drangeklatscht.
    + Fehlende Menüs auf anderen Seiten wurden eingefügt.
    + Stabilitätsverbesserungen und Fehlerkorrekturen

--- Funktionsklassen/Authentifizierung.rb
+++ Funktionsklassen/Authentifizierung.rb
@@ -12,6 +12,7 @@ Schritte des Authorisierungsprozesses
 require 'flickraw'
 require 'launchy'
 
+
 class Authentifizierung
 	  # Diese Werte erhält man direkt von flickr. Sie sind fix.
 	  @flickrApiKey # Jede App hat einen eindeutigen Schlüssel von flickr zugeteilt bekommen.
@@ -23,21 +24,9 @@ class Authentifizierung
 	  @AuthURL
 	  @authTokenFull
 	  @userID
-
-	def initialize()
-		# Diese Werte erhält man direkt von flickr
-		FlickRaw.api_key="88b3faf325e25878ff6c6c6a0d616fc7" # Ist ein fixer Wert der pro Programm einmal vergeben wird. Er ist immer gleich
-		FlickRaw.shared_secret="48f5f79faa401c9f" # Ist ein fixer Wert der pro Programm einmal vergeben wird. Er ist immer gleich
-		@flickrApiKey = "88b3faf325e25878ff6c6c6a0d616fc7"
-		@flickrApiSecret = "48f5f79faa401c9f"
-		
-		#Die User-ID abspeichern
-		tmpUserID = flickr.people.findByUsername("username" => 'susi.sorglos61')
-		@userID = tmpUserID['nsid']
-	end
 	
-=begin
-	def initialize(username)
+
+	def initialize(username="susi.sorglos61")
 		# Diese Werte erhält man direkt von flickr
 		FlickRaw.api_key="88b3faf325e25878ff6c6c6a0d616fc7" # Ist ein fixer Wert der pro Programm einmal vergeben wird. Er ist immer gleich
 		FlickRaw.shared_secret="48f5f79faa401c9f" # Ist ein fixer Wert der pro Programm einmal vergeben wird. Er ist immer gleich
@@ -48,7 +37,7 @@ class Authentifizierung
 		tmpUserID = flickr.people.findByUsername("username" => username)
 		@userID = tmpUserID['nsid']
 	end
-=end
+
 
 	#Nach dem Aufruf von compute soll die Anmeldung stehen
 	#Sie fasst den Prozess in einer Methode zusammen
@@ -59,8 +48,9 @@ class Authentifizierung
 		
 		#Öffnet die Seite für die Authorsierung im Standardbrowser
 		openURI(@AuthURL)
-		puts("Bitte Verifizierungskennung der Webseite hier 1:1 eintippen: ")
-		verify = gets.strip() # Code von der Webseite nach der Genehmigung
+		
+		print("Bitte Verifizierungskennung der Webseite hier 1:1 eintippen: ")
+		verify = gets.strip() # Zahlencode von der Webseite nach der Genehmigung
 
 		begin
 		  flickr.get_access_token(reqToken['oauth_token'], reqToken['oauth_token_secret'], verify)
--- sinatra.rb
+++ sinatra.rb
@@ -1,84 +1,116 @@
-#! /usr/bin/env ruby
-
 require 'flickraw'
 require 'sinatra'
+require 'sinatra/session'
 require 'erb'
 require_relative('Funktionsklassen/Fotozugriff')
 require_relative('Funktionsklassen/Authentifizierung')
 
-# --- Initialisieren ------------------------------------
-# Objekt für den Umgang mit den Fotografien und der Gallerie ------------
+# --- Initialisieren --------------------------------------------------------
+set :session_fail, '/login'
+set :session_secret, 'JanDuhr110'
+
 foto = Fotozugriff.new()
 authObj = Authentifizierung.new()
 # Ruft den Autorisierungsprozess auf
-authObj.compute()
-
-#----------------- Authentication --------------------------------------
-
-FlickRaw.api_key= authObj.getFlickrApiKey()
-FlickRaw.shared_secret= authObj.getFlickrApiSecret()
-
-
-#--------------- Anmelden mit Benutzer ---------------------------------
-
-flickr.access_token = authObj.getFlickrAccesstoken()
-flickr.access_secret = authObj.getFlickrAccessSecret()
-
-
-
-
 
 #--------------Sinatra----------------------------------
-
-get "/login" do
-  erb :login
+get "/" do
+	session!
+	redirect "/gallery"
 end
 
+get "/login" do
+	if session?
+		redirect "/"
+	else
+  		erb :login
+  	end	
+end
+
+post '/login' do
+  if params[:Name]
+    session_start!
+    authObj.compute()
+    session[:name] = params[:Name]
+    redirect '/'
+  else
+    redirect '/login'
+  end
+end
+
+get '/logout' do
+  session_end!
+  redirect '/'
+end
 
 # Zeigt die Gallery vom Benutzer an
 get "/gallery" do
-  @photosInfos = foto.getPhotosInfos()
-    erb :gallery
+	if session?
+  		@photosInfos = foto.getPhotosInfos()
+    	erb :gallery
+    else
+    	redirect "/"
+    end	
 end
 
 
 # Zeigt die Alben vom Benutzer an 
 get "/photosets" do
+if session?
  @photosets = foto.getPhotosets()
   erb :album
+  else
+    	redirect "/"
+    end	
 end
 
 
 # Einzelne Bilder anzeigen in Groß
 get "/photoset/:photosetid" do
+if session?
   @photosetPhotos = foto.getPhotosetPhotos("#{params[:photosetid]}")
    erb :album_gallery
+   else
+    	redirect "/"
+    end	
 end
 
 
 # Einzelne Bilder anzeigen in Groß
 get "/photo/:photoid" do
+if session?
   @photo = flickr.photos.getInfo(:photo_id => "#{params[:photoid]}")
    erb :photo
+   else
+    	redirect "/"
+    end	
 end
 
 
 # Formular für Bilder Hochladen
 get "/upload" do
+if session?
  erb :upload
+ else
+    	redirect "/"
+    end	
 end
 
 
-# Formular für Bilder Hochladen
+# Bild aus dem Stream löschen
 get "/deleted/:photoid" do
+if session?
  foto.deletePicture("#{params[:photoid]}")
- @message="Picture is deleted"
- erb :response
+ redirect "/gallery"
+ else
+    	redirect "/"
+    end	
 end
 
 
 # Bild vom Formular wird auf flickr Hochgeladen
 post "/upload" do
+if session?
  @title = params[:title]
  @pictureLink = params[:pictureLink][:tempfile]
  @description = params[:description]
@@ -86,52 +118,78 @@ post "/upload" do
  foto.uploadPicture(@title,@pictureLink,@description)
 
  redirect "/gallery"
+ else
+    	redirect "/"
+    end	
 end
 
 
 # Formular für Album erstellen
 get "/create_photoset" do
+if session?
  @photosInfos = foto.getPhotosInfos()
  erb :create_photoset
+ else
+    	redirect "/"
+    end	
 end
 
 
 # Album aus Formular wird erstellt
 post "/create_photoset" do
+if session?
  @title = params[:title]
  @primaryPicture = params[:primaryPicture]
  @description = params[:description]
  foto.createPhotoset(@title,@primaryPicture,@description)
  redirect "/photosets"
+ else
+    	redirect "/"
+    end	
 end
 
 
 # Bild aus Album entfernen
 get "/remove/:photosetid/:photoid"  do
+if session?
  foto.removePictureFromPhotoset("#{params[:photosetid]}","#{params[:photoid]}")
- @message="Picture is removed"
- erb :response
+ redirect "/gallery"
+ else
+    	redirect "/"
+    end	
 end
 
 
 # Album Löschen
 get "/delete_photoset/:photosetid"  do
+if session?
  foto.deletePhotoset("#{params[:photosetid]}")
- @message="Photoset is removed"
- erb :response
+ redirect "/gallery"
+ else
+    	redirect "/"
+    end	
+ 
 end
 
 
 # Bilder zum hinzufügen für die Alben anzeigen
 get "/add/:photosetid"  do
+if session?
   @photosInfos = foto.getPhotosInfos()
   @photosetId = "#{params[:photosetid]}"
     erb :add
+    else
+    	redirect "/"
+    end	
 end
 
 
 # Bild zum Album hinzufügen
 get "/add/:photosetid/:photoid"  do
+if session?
   foto.addPhotoToPhotoset("#{params[:photosetid]}","#{params[:photoid]}")
     redirect "/photoset/#{params[:photosetid]}"
+    else
+    	redirect "/"
+    end	
 end
